1be32aebdc76f36226c66036a691455d03e0acd3
rikakohira <rikako.takase88@gmail.com>
2022-10-25 10:14:58 +0900

    Studentクラス作成

--- Student.java
+++ Student.java
@@ -1,3 +1,11 @@
-public class Student {
+public class Studen extends Person {
+  private int stuNo;
 
+  public void setStuNo(int s) {
+    netNo = s;
+  }
+
+  public void displayStuNo() {
+    System.out.println("学籍番号：" + stuNo);
+  }
 }
