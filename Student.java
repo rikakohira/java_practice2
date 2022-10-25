f3c3f1095483c76ed0b2390a99850c58e40ed5c2
rikakohira <rikako.takase88@gmail.com>
2022-10-25 16:27:15 +0900

    Studentクラス

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-public class Student extends Person {
+public final class Student extends Person {
   // private int stuNo;
 
   // public void setStuNo(int s) {
@@ -19,20 +19,33 @@ public class Student extends Person {
   //   System.out.println("学籍番号：" + stuNo);
   // }
 
+  // private int stuNo;
+
+  // public Student(String name) {
+  //   this(name, 999);
+  // }
+
+  // public Student(String name, int stuNo){
+  //   super(name);
+  //   this.stuNo = stuNo;
+  // }
+
+  // public void display() {
+  //   super.display();
+  //  
+
   private int stuNo;
-
-  public Student(String name) {
-    this(name, 999);
-  }
-
-  public Student(String name, int stuNo){
-    super(name);
+  
+  public Student(String name, int stuNo){  // 菅原, 1001
+    super(name); 
     this.stuNo = stuNo;
-  }
+  } 
 
   public void display() {
     super.display();
     System.out.println("学籍番号：" + stuNo);
   }
+  
 }
 
+  
