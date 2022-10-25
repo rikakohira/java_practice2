63c06a232ed58b700138b528a05973c706d7052d
rikakohira <rikako.takase88@gmail.com>
2022-10-25 16:27:08 +0900

    Personクラス

--- Person.java
+++ Person.java
@@ -2,11 +2,11 @@ public class Person {
   // private String name;
 
   // public void setName(String n) {
-  // name = n;
+  // me = n;
   // }
 
   // public void display() {
-  // System.out.println("名前：" + name);
+  // stem.out.println("名前：" + name);
   // }
 
   // private String name;
@@ -23,13 +23,23 @@ public class Person {
   //   System.out.println("名前：" + name);
   // }
 
-  private String name;
+  // private String name;
+
+  // public Person(String name){
+  //   this.name = name;
+  // }
+
+  // public void display(){
+  //   System.out.println("名前：" + name);
+  // }
+
+  private String name;  // 菅原
 
   public Person(String name){
     this.name = name;
   }
-
   public void display(){
     System.out.println("名前：" + name);
   }
+
 }
