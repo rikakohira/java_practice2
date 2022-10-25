3d7bf6ec134ad452a19b19dcc90f7127aca9f8ed
rikakohira <rikako.takase88@gmail.com>
2022-10-25 10:55:26 +0900

    オーバーライド

--- Person.java
+++ Person.java
@@ -1,11 +1,26 @@
 public class Person {
+  // private String name;
+
+  // public void setName(String n) {
+  // name = n;
+  // }
+
+  // public void display() {
+  // System.out.println("名前：" + name);
+  // }
+
   private String name;
 
   public void setName(String n) {
     name = n;
   }
 
+  public String getName() {
+    return name;
+  }
+
   public void display() {
     System.out.println("名前：" + name);
   }
+
 }
