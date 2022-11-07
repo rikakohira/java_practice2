87d404a6c2817ecccbe75247de72eedfd9d30787
rikakohira <rikako.takase88@gmail.com>
2022-11-07 22:15:01 +0900

    Carクラス作成

---
+++ Car.java
@@ -0,0 +1,20 @@
+public class Car {
+  private int no;
+
+  public void setNo(int n) {
+    no = n;
+  }
+
+  public void display() {
+    System.out.println("ナンバーは" + no + "です");
+  }
+
+}
+
+  
+
+  
+
+  
+
+  
