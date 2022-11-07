6d84a15c7795d1663293d824c6fc611bb7dd387d
rikakohira <rikako.takase88@gmail.com>
2022-11-07 23:10:23 +0900

    実行用クラス

---
+++ DriveTaxi1.java
@@ -0,0 +1,13 @@
+public class DriveTaxi1 {
+  public static void main(String[] args) {
+    Car1 c = new Car1();
+    System.out.println("----");
+    Taxi1 t1 = new Taxi1(2525);
+    System.out.println("----");
+    Taxi1 t0 = new Taxi1();
+    System.out.println("----");
+    Taxi1 t2 = new Taxi1("２５２５");
+
+  }
+
+}
