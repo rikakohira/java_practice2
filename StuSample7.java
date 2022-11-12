7ec9cf944f99566cd5890b9ede3e41522222b9f2
rikakohira <rikako.takase88@gmail.com>
2022-11-12 15:19:14 +0900

    Create: StuSample7

---
+++ StuSample7.java
@@ -0,0 +1,10 @@
+public class StuSample7 {
+  public static void main(String[] args) {
+    Student7 stu = new Student7("Mike");
+    stu.displayEng();
+
+    Baseball bb = new Baseball();
+    bb.displayEng();
+  }
+
+}
