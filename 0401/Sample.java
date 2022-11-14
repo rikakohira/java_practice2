98d032b2d3bbbb44fccd1b07c887d7fd6d28b4a3
rikakohira <rikako.takase88@gmail.com>
2022-11-14 22:51:02 +0900

    commit

--- 0401/Sample.java
+++ 0401/Sample.java
@@ -2,9 +2,11 @@ public class Sample {
   public static void main(String[] args) {
     Taxi taxi = new Taxi(5);
     Train train = new Train(2);
+    Bus bus = new Bus(8);
 
     taxi.showCrewNum();
     train.showCrewNum();
+    bus.showCrewNum();
   }
 
 }
