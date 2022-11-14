a58a40d159f87d2873cff1b46589035e815958c6
rikakohira <rikako.takase88@gmail.com>
2022-11-14 22:34:14 +0900

    Create: Sample

---
+++ 0401/Sample.java
@@ -0,0 +1,10 @@
+public class Sample {
+  public static void main(String[] args) {
+    Taxi taxi = new Taxi(5);
+    Train train = new Train(2);
+
+    taxi.showCrewNum();
+    train.showCrewNum();
+  }
+
+}
